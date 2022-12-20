42c2593e381e8f36a45f304a6c94dd43b892e096
partharora2233@gmail.com <partharora2233@gmail.com>
2022-12-21 00:31:33 +0530

    replace placeholder function created

--- index.js
+++ index.js
@@ -1,24 +1,64 @@
 const fs = require("fs");
 const http = require("http");
-const path = require("path");
+// const path = require("path");
 const url = require("url");
 
+// no need to read the data everytime we can just sync read it once in the start and use that data
+
+// readling file in the memory
 const data = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
-const dataobj = JSON.parse(data);
+const dataObj = JSON.parse(data);
+const overviewTemplate = fs.readFileSync(
+  `${__dirname}/template/template-overview.html`,
+  "utf-8"
+);
+
+const productTemplate = fs.readFileSync(
+  `${__dirname}/template/template-product.html`,
+  "utf-8"
+);
+
+const cardTemplate = fs.readFileSync(
+  `${__dirname}/template/template-card.html`,
+  "utf-8"
+);
+
+const replaceTemplate = function (temp, product) {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%FROM%}/g, product.from);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
 
 //////////////////////////////////////////////
 // SERVER
 const server = http.createServer((req, res) => {
   const pathName = req.url;
-
+  // Overview Page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
-  }
+    res.writeHead(200, { "Content-type": "text/html" });
 
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(cardTemplate, el))
+      .join("");
+
+    const output = overviewTemplate.replace(/{%PRODUCTCARDS%}/g, cardsHtml);
+    res.end(output);
+  }
+  // Product Page
   if (pathName === "/product") {
     res.end("This is the PRODUCT");
   }
-
+  // API
   if (pathName === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
